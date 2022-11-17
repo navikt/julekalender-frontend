1fdccd1dac7a1904b8f7ceb7becb1464c57aee1f
Kent D <kent.daleng@nav.no>
2022-11-17 18:04:08 +0100

    start saving state to localstorage

--- components/boardSquare.tsx
+++ components/boardSquare.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@navikt/ds-react"
 
-const BoardSquare = ({value, index, onClick, disabled, completed, committed}: {value: string, index: number, disabled: boolean, completed: boolean, committed: boolean, onClick: (value: string) => void}) => {
+const BoardSquare = ({value, index, onClick, completed, committed}: {value: string, index: number, completed: boolean, committed: boolean, onClick: (value: string) => void}) => {
     return <Button
             className={`rounded-md p-4 text-lg border disabled:opacity-50 ${(completed || committed) && "bg-red-600 text-white"}`}
             value={index.toString()}
-            disabled={disabled || committed}
+            disabled={committed}
             onClick={() => onClick(index.toString())}>
             {value}
         </Button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,51 +1,64 @@
-import { Button } from "@navikt/ds-react"
+import { Alert, Button, Heading } from "@navikt/ds-react"
 import { useEffect, useState } from "react"
 import BoardSquare from "../components/boardSquare"
 
-// get these from the backend?
 const squares = [
-    "noe gøy",
-    "noe litt vanskelig",
-    "dra på kontoret",
-    "ta et artig bilde",
-    "drikk vann",
-    "hør på julemusikk",
-    "spis en klementin",
-    "noe umulig",
-    "før i minwintid at du starter, når du starter (umulig)",
-    "noe vanskelig",
-    "noe koselig",
-    "ta bilde av noe kult ute",
-    "stå på hendene",
-    "dra til sunnmøre",
-    "spill et (brett/video)spill",
-    "rydd skrivebordet hjemme :)",
-    "dans en dans",
-    "gå 1000 skritt hver 3. time",
-    "vaske golvet",
-    "bære ved",
-    "sett opp fugglebånd",
-    "pynt tre",
-    "sett deg ned og hvil",
-    "rugge vogna",
+    "oppgave en",
+    "oppgave to",
+    "oppgave tre",
+    "oppgave fire",
+    "oppgave fem",
+    "oppgave seks",
+    "oppgave syv",
+    "oppgave åtte",
+    "oppgave ni",
+    "oppgave ti (en oppgave med spesielt lang tekst, det kan se rart ut men kanskje ikke?)",
+    "oppgave elleve",
+    "oppgave tolv",
+    "oppgave tretten",
+    "oppgave fjorten",
+    "oppgave femten",
+    "oppgave seksten"
 ]
+
+const load = (key: string): string | null => {
+    if(typeof window !== 'undefined'){
+         return window.localStorage.getItem(key)
+    }
+    return null
+}
+
+const save = (key: string, value: string) => {
+    if(typeof window !== 'undefined'){
+         return window.localStorage.setItem(key,value)
+    }
+}
+    
 
 export default function Home() {
 
-    const [disabled, setDisabled] = useState(false)
     const [boardState, setBoardState] = useState<string[]>([])
     const [committedState, setCommittedState] = useState<string[]>([])
+    const [score, setScore] = useState<number>(0)
 
     useEffect(() => {
         // fetch committed state from backend
-        // setCommittedState(remoteBoardState)
-        setBoardState(committedState) // put committed state into our "hot" state for easier comparisons later
+        const board = load("board-1")
+        const parsed: string[] = board ? JSON.parse(board) : []
+        setCommittedState(parsed)
+        setBoardState(parsed)
+         // put committed state into our "hot" state for easier comparisons later
     }, [])
+
+    useEffect(() => {
+        setScore(committedState.length)
+    }, [committedState])
 
 
     const commitState = () => {
         // post state to backend
-        setCommittedState(boardState) // we might as well just set committed state the same as the bingo state
+        save("board-1", JSON.stringify(boardState))
+        setCommittedState(boardState)
     }
 
     const handleChange = (square: string) => {
@@ -54,14 +67,18 @@ export default function Home() {
             : setBoardState(boardState.filter(item => item != square)) // remove square :)
     }
 
-    return (<div className="flex flex-col max-w-2xl gap-4 pb-48">
-        <div className="flex flex-row flex-wrap gap-2 px-2 max-w-xl">
+    return (<div className="flex flex-col max-w-2xl gap-4 pb-48 p-4">
+        <h1 className="text-3xl">Aktivitetskalender (1. desember - 8. desember)</h1>
+        <p className="text-gray-300 p-2 border-l-2">
+            Det er flere oppgaver her enn det er dager i perioden, det er ikke meningen at du skal gjøre alle (men du kan hvis du vil!).
+            På torsdag 8. desember oppfordrer vi deg til å sende inn svar i Forms-skjemaet vårt (link her) for å kunne bli med i trekning av noe godt. :)
+        </p>
+        <div className="flex flex-row flex-wrap gap-2 py-4 p-2 max-w-xl">
             {squares.map((square, index) => 
                 <BoardSquare 
                     key={index}
                     value={square}
                     index={index}
-                    disabled={disabled}
                     committed={committedState.includes(index.toString())}
                     completed={boardState.includes(index.toString())}
                     onClick={handleChange} 
@@ -69,9 +86,14 @@ export default function Home() {
             )}
         
     </div>
+    <div className="text-xl">
+        Utførte oppgaver: {score}
+    </div>
+    { boardState.length != squares.length &&
     <Button onClick={commitState} 
         className="bg-green-800 hover:bg-green-900 transition-all duration-300 opacity-90 h-24 text-xl self-auto fixed bottom-0 left-0 w-full">
             lagre
     </Button>
+    }
     </div>)
 }
