1e855b167d70a414d7814460e384958d9dc45310
Kent D <kent.daleng@nav.no>
2022-11-24 08:26:51 +0100

    add key

--- components/snowflakes.tsx
+++ components/snowflakes.tsx
@@ -2,7 +2,7 @@
 const Snowflakes = ({flakes}: {flakes: number}) => {
     return <div aria-hidden="true">
         {[...Array(flakes)].map((_, i) => 
-            <div className={`snowflake drop-shadow-[0_2px_2px_rgba(255,255,255,0.25)]`}><img src="/snø.svg" /></div>
+            <div key={i} className={`snowflake drop-shadow-[0_2px_2px_rgba(255,255,255,0.25)]`}><img src="/snø.svg" /></div>
         )}
     </div>
 }
