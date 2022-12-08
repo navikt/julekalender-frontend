1990b3a9b37a9346d41e5efb9f532a5361e4b2aa
Kent D <kent.daleng@nav.no>
2022-12-08 15:13:41 +0100

    prepare next board

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,72 +20,56 @@ const unscramble = (str: string, salt: number) => scramble(str, salt, -1);
 
 const squares = [
   {
-    name: "Ta trappa opp til takterrassen og trekk litt frisk luft",
-    description: "Fantastisk utsikt og frisk pust!",
-  },
-  {
-    name: "Gå av kollektivtransport et stopp for tidlig",
-    description: "Alle monner drar!",
-  },
-  {
-    name: "10 000 skritt på en dag",
-    description:
-      "Tilsvarer omtrent 8 kilometer, eller ca. 1 time og 40 minutter",
-  },
-  {
     name: "Øve på å stå på hendene eller hodet",
     description:
       "Målet er å klare det innen 24. desember. Du har fullført aktiviteten om du har øvd litt.",
   },
-  { name: "5 kilometer til fots", description: "Heia heia!" },
-  {
-    name: "Gå en tur med en kollega",
-    description: "Frisk luft er godt for både kropp og sinn!",
-  },
   {
     name: "Gå eller løp antallet km som datoen i dag",
     description:
-      "Dersom du gjør dette 1. desember trenger du kun å gå eller løpe en kilometer, men dersom du gjør dette 8. desember er distansen 8 kilometer. God tur! PS: Dette er en av to identiske oppgaver.",
-  },
-
-  { name: "Dans sammen med noen", description: "Ta deg en svingom!" },
-  {
-    name: "Besøk en etasje i FYA1 du ikke har vært i før",
-    description: "Har du vært i alle?  Velg den som er lengst unna deg nå!",
-  },
-  {
-    name: "Gå eller løp antallet km som datoen i dag",
-    description:
-      "Dersom du gjør dette 1. desember trenger du kun å gå eller løpe en kilometer, men dersom du gjør dette 8. desember er distansen 8 kilometer. God tur! PS: Dette er en av to identiske oppgaver.",
-  },
-  {
-    name: "Gå en morgentur",
-    description: "Ta deg en tur før lunsj!",
+      "Dersom du gjør dette 1. desember trenger du kun å gå eller løpe en kilometer, men dersom du gjør dette 8. desember er distansen 8 kilometer. God tur!",
   },
   { name: "Heis-fri dag", description: "Spar strøm og bli venn med trappa!" },
-
   {
-    name: "Gå tur mens du hører en episode av IT Simpelthen",
-    description: (
-      <p>
-        Du kan høre på den siste episoden, eller en valgfri episode!{" "}
-        <a href="https://share.transistor.fm/e/it-simpelthen/playlist/dark">
-          Her finner du en oversikt over episodene til IT Simpelthen.
-        </a>
-      </p>
-    ),
+    name: "10 000 skritt på en dag",
+    description: "Tilsvarer omtrent 8 kilometer, eller ca. 1 time og 40 minutter",
   },
-
+  {
+    name: "Gjør planken så lenge du klarer",
+    description: "Målet er å kunne gjøre det enda lengre neste uke!",
+  },
+  { name: "10 kilometer til fots", description: "Heia heia!" },  
+  {
+    name: "10 burpies eller spensthopp",
+    description: ""
+  },
+  {
+    name: "Gå eller løp ei rute du aldri har gjort før",
+    description: "God tur!"
+  },
   {
     name: "Ta et utendørsbad",
     description: "Hopp i havet! Eller ferskvannet!",
+  },
+  {
+    name: "Gå eller løp sammen med en kollega",
+    description: "Frisk luft er godt for både kropp og sinn!",
+  },
+
+  {
+    name: "Bli med på morgenmeditasjon, mandag",
+    description: (
+      <p>
+        Vi møtes i B110 Svovelstikken i Fyrstikkaleen 1, <b>mandag 12. desember klokken 08:45</b>. Varighet: 5 minutter!
+      </p>
+    )
   },
   {
     name: "Bli med løpegruppa på tur, mandag",
     description: (
       <p>
         Vi møtes i resepsjonen i 1. etasje i Fyrstikkalleen 1,{" "}
-        <b>mandag 5. desember klokken 16:30</b>, for alle nivåer! Bli med i{" "}
+        <b>mandag 12. desember klokken 16:30</b>, for alle nivåer! Bli med i{" "}
         <a href="https://nav-it.slack.com/archives/C02L82FACMN">#fa1-løping</a>{" "}
         på Slack hvis du vil!
       </p>
@@ -95,7 +79,7 @@ const squares = [
     name: "Rolig yoga, tirsdag",
     description: (
       <p>
-        Skjer i Smeltedigelen i FYA1 på <b>tirsdag 6. desember, 16:30–⁠17:30</b>
+        Skjer i Smeltedigelen i FYA1 på <b>tirsdag 13. desember, 16:30–⁠17:30</b>
         . Alle har en gratis prøvetime. Husk komfortable klær du kan bevege deg
         fritt i og varm genser og sokker til avspenningen. Gi gjerne beskjed om
         at du kommer til {unscramble("udm5ixo{ndurAqd}/qr", 1337)}
@@ -103,57 +87,34 @@ const squares = [
     ),
   },
   {
-    name: "Morgenbuldring med klatregruppa, onsdag",
-    description: (
-      <Chat avatar="K" name="Klatregruppa" timestamp="07.12.2022 07:00">
-        <Chat.Bubble className="font-bold">
-          Oppmøte 07:00, onsdag 7. desember
-        </Chat.Bubble>
-        <Chat.Bubble>
-          Adresse: Brynsveien 3, Klatreverket Bryn (Buldreverket)
-        </Chat.Bubble>
-        <Chat.Bubble>Pris: 100 kr</Chat.Bubble>
-        <Chat.Bubble>Mulighet for å leie sko</Chat.Bubble>
-        <Chat.Bubble>Plass til alle, både n00b og pr0</Chat.Bubble>
-        <Chat.Bubble>Kaffe :)</Chat.Bubble>
-      </Chat>
-    ),
-  },
-  {
-    name: "Go-morgon med feittforbrenningsgaranti, torsdag",
+    name: "Go-morgon med fettforbrenningsgaranti, torsdag",
     description: (
       <div className="flex flex-col gap-2">
         <p>Frode Lein tek oss med på intervalltrening!</p>
         <p>
-          Oppmøte ved hovudinngangen til FYA1 ferdig påkledd klar for løpetrening ute, <b>torsdag 8. desember, 07:00–08:00</b>. 
+          Oppmøte ved hovudinngangen til Fyrstikkalleen 1 ferdig påkledd klar for løpetrening ute, <b>torsdag 15. desember, 07:00–08:00</b>. 
           Vi starter med femten minutt roleg oppvarming og deretter intervall der vi samlast i pausane.
         </p>
         <p>
-          Intervalløkt den 8. desember vil vere:
+          Intervalløkt den 15. desember vil vere:
         </p>
         <ul className="list-disc pl-4">
           <li>Oppvarming (15 minutt)</li>
-          <li>Springe raskt (4 minutt)</li>
-          <li>Pause (2 minutt)</li>
-          <li>Springe 4 gonger, 45 sekund raskt med 15 sekund kvile mellom kvart &ldquo;drag&rdquo; (totalt 4 minutt)</li>
-          <li>Pause (2 minutt)</li>
-          <li>Springe raskt (4 minutt)</li>
-          <li>Pause (2 minutt)</li>
-          <li>Springe 4 gonger, 45 sekund raskt med 15 sekund kvile mellom kvart &ldquo;drag&rdquo; (totalt 4 minutt)</li>
-          <li>Pause (2 minutt)</li>
-          <li>Springe raskt (4 minutt)</li>
+          <li>Springe raskt 2 gonger 4 minutt med 120 sekund kvile mellom kvart &ldquo;drag&rdquo;</li>
+          <li>Springe raskt 2 gonger 3 minutt med 90 sekund kvile mellom kvart &ldquo;drag&rdquo;</li>
+          <li>Springe raskt 2 gonger 2 minutt med 60 sekund kvile mellom kvart &ldquo;drag&rdquo;</li>
+          <li>Springe raskt 4 gonger 1 minutt med 30 sekund kvile mellom kvart &ldquo;drag&rdquo;</li>
           <li>Nedvarming (15 minutt)</li>
         </ul>
         <p>Ikkje start for hardt. Du skal klare å halda farta oppe på alle drag inkludert det siste. Dette er ein effektiv treningsform som aukar både uthald og kaloriforbruk. I tillegg forbrenn du meir feitt og reduserer risikoen for mange kroniske sjukdommar.</p>
       </div>
-    ),
+    )
   },
-
   {
     name: "Mensendieck, torsdag",
     description: (
       <p>
-        Skjer i Smeltedigelen i FYA1 på <b>torsdag 8. desember, 16:30–⁠17:30</b>
+        Skjer i Smeltedigelen i FYA1 på <b>torsdag 15. desember, 16:30–⁠17:30</b>
         . Alle har en gratis prøvetime. Husk treningstøy og sko til innebruk. Gi
         gjerne beskjed om at du kommer til {unscramble("uru|oq1kpqyplCqhw1qv", 1337)}
       </p>
@@ -168,7 +129,7 @@ export default function Home() {
   const [openSquare, setOpenSquare] = useState(squares.map((_) => false));
   const [save, load] = useLocalStorage();
 
-  const boards = ["board-1"];
+  const boards = ["board-1", "board-2"];
   const introState = "intros";
   const currentBoard = boards.slice(-1)[0];
 
@@ -207,7 +168,7 @@ export default function Home() {
         <Intro currentBoard={currentBoard} />
         <div className="flex flex-col max-w-2xl gap-4 pb-48 p-4">
           <h1 className="text-3xl">
-            Aktivitetskalender (1. desember - 8. desember)
+            Aktivitetskalender (9. desember - 15. desember)
           </h1>
           <div className="p-2 border-l-2 border-nav-red flex flex-col gap-2">
             <p>
@@ -216,7 +177,7 @@ export default function Home() {
               aktiviteter som kan løsne litt på både kropp og sinn.
             </p>
             <p>
-              I aktivitetsjulekalender finner du aktiviteter som kan få deg
+              I aktivitetsjulekalenderen finner du aktiviteter som kan få deg
               gjennom julestria med masse energi og påfyll. Det spiller ingen
               rolle om du er sofasliter, supermosjonist eller sånn passe
               energisk, det er noe for alle her.
@@ -229,7 +190,7 @@ export default function Home() {
               <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=NGU2YsMeYkmIaZtVNSedC7UZM7Z2a_tDhAO6kE2Ce0dUOTExT1Y3MTA1VFpTMlNPOElHVlpMWjNJSS4u">
                 fylle ut dette skjemaet
               </a>{" "}
-              i løpet av torsdag 8. desember. Vinnerne trekkes fredags morgen og
+              i løpet av torsdag 15. desember. Vinnerne trekkes fredags morgen og
               blir kontaktet direkte.
             </p>
           </div>
